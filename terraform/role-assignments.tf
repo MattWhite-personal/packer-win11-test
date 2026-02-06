69f61b0d9086db183c188609d8d2a0898a24ac18
Matt White <matt@matthewjwhite.co.uk>
2026-02-06 21:31:37 +0000

    remove role assignments as likely not required

--- terraform/role-assignments.tf
+++ terraform/role-assignments.tf
@@ -1,5 +1,5 @@
 # Role assignments for Packer and GitHub Actions service principals
-
+/*
 # Role assignment for Packer SP: Contributor on the resource group
 resource "azurerm_role_assignment" "packer_sp_contributor" {
   scope                = azurerm_resource_group.image-gallery.id
@@ -20,3 +20,4 @@ resource "azurerm_role_assignment" "github_actions_subscription" {
   role_definition_name = "Contributor"
   principal_id         = var.github_actions_sp_object_id
 }
+*/
